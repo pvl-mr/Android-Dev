cf27cfced1434cea2ec66999058aa3d196f6497e
pvl-mr <mariyapavlova2001@mail.ru>
2021-11-05 15:04:14 +0400

    3 lab is working in 92% cases

--- app/src/main/java/com/example/myapplication2/default_fragment.java
+++ app/src/main/java/com/example/myapplication2/default_fragment.java
@@ -1,10 +1,13 @@
 package com.example.myapplication2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,16 +18,25 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class default_fragment extends Fragment {
 
-    public static final ArrayList<String> names = new ArrayList<>();
-    ArrayList<String> selectedNames = new ArrayList<>();
-    public static ArrayAdapter<String> adapter;
+    public static ArrayList<Telephone> names = new ArrayList<>();
+    ArrayList<Telephone> selectedNames = new ArrayList<>();
+    public static ArrayAdapter<Telephone> adapter;
     ListView namesList;
     Button buttonAllElem;
     Button buttonSearch;
@@ -33,7 +45,13 @@ public class default_fragment extends Fragment {
     Button buttonDelete;
     Button buttonElemInToast;
     Button buttonEdit;
+    Button buttonSaveAll;
+    Button buttonGetAll;
+    Button buttonImport;
     EditText searchObj;
+    private SharedPreferences pref;
+    private final String save_key = "save_key";
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,7 +59,7 @@ public class default_fragment extends Fragment {
         // Inflate the layout for this fragment
         super.onCreate(savedInstanceState);
 
-        View view = inflater.inflate(R.layout.fragment_default_fragment, container, false);
+        view = inflater.inflate(R.layout.fragment_default_fragment, container, false);
 
         // получаем элемент ListView
         namesList = view.findViewById(R.id.namesList);
@@ -58,11 +76,11 @@ public class default_fragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 //получаем нажатый элемент
-                String name = adapter.getItem(position);
+                Telephone tel = adapter.getItem(position);
                 if (namesList.isItemChecked(position)) {
-                    selectedNames.add(name);
+                    selectedNames.add(tel);
                 } else {
-                    selectedNames.remove(name);
+                    selectedNames.remove(tel);
                 }
 
             }
@@ -76,6 +94,9 @@ public class default_fragment extends Fragment {
         buttonDelete = view.findViewById(R.id.buttonDelete);
         buttonElemInToast = view.findViewById(R.id.buttonElemInToast);
         buttonEdit = view.findViewById(R.id.buttonEdit);
+        buttonSaveAll = view.findViewById(R.id.buttonSaveAll);
+        buttonGetAll = view.findViewById(R.id.buttonGetData);
+        buttonImport = view.findViewById(R.id.buttonImport);
 
         buttonAllElem.setOnClickListener(v -> {
             selectAllElem();
@@ -92,8 +113,8 @@ public class default_fragment extends Fragment {
             ArrayList<String> searchResultString = new ArrayList<>();
 
             for (int i = 0; i < names.size(); i++) {
-                if (names.get(i).toLowerCase().contains(searchString)) {
-                    searchResultString.add(names.get(i));
+                if (names.get(i).getName().toLowerCase().contains(searchString)) {
+                    searchResultString.add(names.get(i).getName());
                     System.out.println("NAMES " + names.get(i));
                 }
             }
@@ -126,16 +147,35 @@ public class default_fragment extends Fragment {
         buttonEdit.setOnClickListener(v -> {
             FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            Log.d("selected-names", names.indexOf(selectedNames.get(0))+"");
             NewFragment editFragment = new NewFragment(selectedNames.get(0), names.indexOf(selectedNames.get(0)));
             fragmentTransaction.replace(R.id.default_fragment, editFragment, "tag");
             fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
+            namesList.setItemChecked(names.indexOf(selectedNames.get(0)), false);
+            selectedNames.clear();
+        });
+        buttonSaveAll.setOnClickListener(v -> {
+            saveAll();
+        });
+
+        buttonSaveAll.setOnClickListener(v -> {
+            saveAll();
+        });
+
+        buttonGetAll.setOnClickListener(v -> {
+            getAll();
+        });
+
+        buttonImport.setOnClickListener(v -> {
+            importData();
         });
         return view;
     }
 
     public void selectAllElem(){
         for(int i = 0; i < names.size(); i++) {
+            selectedNames.add(names.get(i));
             namesList.setItemChecked(i, true);
         }
     }
@@ -166,11 +206,81 @@ public class default_fragment extends Fragment {
             }
 
     public void remove(){
-        for(int i=0; i< selectedNames.size();i++){
+        Log.d("size ---", selectedNames.size()+"");
+        for(int i=0; i < selectedNames.size();i++){
             adapter.remove(selectedNames.get(i));
+            Log.d("selected obj ---", selectedNames.get(i).toString());
         }
         namesList.clearChoices();
         selectedNames.clear();
         adapter.notifyDataSetChanged();
+    }
+
+    private void saveAll() {
+        pref = getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        Gson gson = new Gson();
+        editor.putInt("save_key_count", names.size());
+        for (int i = 0; i < names.size(); i++) {
+            String myData = gson.toJson(names.get(i));
+            editor.putString(save_key + i + "", myData);
+        }
+        editor.apply();
+    }
+
+    private void getAll() {
+        if (pref == null)
+            return;
+        Gson gson = new Gson();
+        ArrayList<Telephone> telList= new ArrayList<>();
+        Log.d("save-key ", save_key);
+        Integer count = pref.getInt("save_key_count", 0);
+        for (int i = 0; i < count; i++) {
+            String data = pref.getString( save_key + i + "", "empty");
+            telList.add(gson.fromJson(data, Telephone.class));
+        }
+        adapter = new ArrayAdapter(view.getContext(),
+                android.R.layout.simple_list_item_multiple_choice, telList);
+        namesList.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        Log.d("telephone size ", telList.size()+"");
+        for (Telephone telephone: telList) {
+            Log.d("telephone ", telephone.toString());
+        }
+        Log.d("telephone ", "--------------------");
+    }
+
+    private void importData() {
+        String jsonString;
+        try {
+            InputStream is = view.getContext().getAssets().open("data.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            jsonString = new String(buffer, StandardCharsets.UTF_8);
+            Log.d("DATA!!!!", jsonString);
+            JSONObject jsonObject = new JSONObject(jsonString);
+            Log.d("temp name", jsonObject.toString());
+            JSONArray jsonArray = jsonObject.getJSONArray("telephones");
+            Log.d("temp name", jsonArray.toString());
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject obj = jsonArray.getJSONObject(i);
+                Telephone temp = new Telephone();
+                Log.d("temp name", obj.getString("name"));
+                Log.d("temp price", obj.getInt("price")+"");
+                Log.d("temp boolean", obj.getBoolean("isSelected")+"");
+                temp.setName(obj.getString("name"));
+                temp.setPrice(obj.getInt("price"));
+                temp.setChecked(obj.getBoolean("isSelected"));
+                names.add(temp);
+                adapter.notifyDataSetChanged();
+            }
+
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();        }
+
+
     }
 }
